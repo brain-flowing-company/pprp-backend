4b10dc3959b8e6fe4312b043fae1ddb000ae8385
Thanapat Ussawanarong <thanapat.usw@gmail.com>
2024-01-29 20:58:17 +0700

    feat: json formatting

--- internal/models/property.model.go
+++ internal/models/property.model.go
@@ -3,39 +3,39 @@ package models
 import "time"
 
 type Property struct {
-	PropertyId            string
-	Description           string
-	ResidentialType       string
-	ProjectName           string
-	Address               string
-	Alley                 string
-	Street                string
-	SubDistrict           string
-	District              string
-	Province              string
-	Country               string
-	PostalCode            string
-	PropertyListTimestamp time.Time
-	PropertyImages        []PropertyImage `gorm:"references:PropertyId"`
-	SellingProperty       SellingProperty `gorm:"references:PropertyId"`
-	RentingProperty       RentingProperty `gorm:"references:PropertyId"`
+	PropertyId            string          `json:"property_id"`
+	Description           string          `json:"description"`
+	ResidentialType       string          `json:"residential_type"`
+	ProjectName           string          `json:"project_name"`
+	Address               string          `json:"address"`
+	Alley                 string          `json:"alley"`
+	Street                string          `json:"street"`
+	SubDistrict           string          `json:"sub_district"`
+	District              string          `json:"district"`
+	Province              string          `json:"province"`
+	Country               string          `json:"country"`
+	PostalCode            string          `json:"postal_code"`
+	PropertyListTimestamp time.Time       `json:"property_list_time_stamp"`
+	PropertyImages        []PropertyImage `gorm:"references:PropertyId" json:"images"`
+	SellingProperty       SellingProperty `gorm:"references:PropertyId" json:"selling"`
+	RentingProperty       RentingProperty `gorm:"references:PropertyId" json:"renting"`
 }
 
 type PropertyImage struct {
-	PropertyId string
-	ImageUrl   string
+	PropertyId string `json:"-"`
+	ImageUrl   string `json:"url"`
 }
 
 type SellingProperty struct {
-	PropertyId string
-	Price      float64
-	IsSold     bool
+	PropertyId string  `json:"-"`
+	Price      float64 `json:"price"`
+	IsSold     bool    `json:"is_sold"`
 }
 
 type RentingProperty struct {
-	PropertyId    string
-	PricePerMonth float64
-	IsOccupied    bool
+	PropertyId    string  `json:"-"`
+	PricePerMonth float64 `json:"price_per_month"`
+	IsOccupied    bool    `json:"is_occupied"`
 }
 
 func (p Property) TableName() string {
