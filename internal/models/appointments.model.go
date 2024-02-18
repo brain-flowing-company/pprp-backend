68e6cfa958171ecf8b7b8fcc18a7181a4e9beebd
Thanapat Ussawanarong <thanapat.usw@gmail.com>
2024-02-18 12:07:00 +0700

    fix: model example

--- internal/models/appointments.model.go
+++ internal/models/appointments.model.go
@@ -21,7 +21,7 @@ type CreatingAppointments struct {
 	PropertyId       uuid.UUID   `json:"property_id"       example:"123e4567-e89b-12d3-a456-426614174000"`
 	OwnerUserId      uuid.UUID   `json:"owner_user_id"     example:"123e4567-e89b-12d3-a456-426614174000"`
 	DwellerUserId    uuid.UUID   `json:"dweller_user_id"   example:"123e4567-e89b-12d3-a456-426614174000"`
-	AppointmentDates []time.Time `json:"appointment_dates" example:"123e4567-e89b-12d3-a456-426614174000"`
+	AppointmentDates []time.Time `json:"appointment_dates" example:"2024-02-18T11:00:00Z"`
 }
 
 type DeletingAppointments struct {
