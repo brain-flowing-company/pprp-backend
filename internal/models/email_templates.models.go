492da5c68a013f506deb37ef13897918f17581bf
Thanapat Ussawanarong <thanapat.usw@gmail.com>
2024-02-24 00:10:25 +0700

    fix: typo

--- internal/models/email_templates.models.go
+++ internal/models/email_templates.models.go
@@ -13,5 +13,5 @@ type VerificationEmails struct {
 }
 
 func (v VerificationEmails) Path() string {
-	return "/internal/templates/VerificationEmail.html"
+	return "internal/templates/VerificationEmail.html"
 }
