c1414da540821d034f311b065fb8528359bc05ad
Thanapat Ussawanarong <thanapat.usw@gmail.com>
2024-02-25 23:21:21 +0700

    feat: test frontend

--- internal/core/chats/public/index.js
+++ internal/core/chats/public/index.js
@@ -1,69 +1,123 @@
 var conn;
+var message_pane = document.getElementById("messages");
+var message = document.getElementById("message");
+
+message.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    send();
+  }
+});
 
 function send() {
-  if (!conn) {
-    return false;
-  }
-
-  if (!msg.value) {
-    return false;
-  }
+  if (!conn || !message.value) return;
 
   conn.send(
     JSON.stringify({
-      content: msg.value,
-      receiver_id: document.getElementById("chat-user-id").value,
+      content: message.value,
       sent_at: new Date(Date.now()).toISOString(),
-      tag: "3o8qtahiubfv3kuwefbis",
+      tag: `tag=${Math.random().toString(16).substring(2)}`,
     })
   );
-  msg.value = "";
 
-  return false;
+  message.value = "";
+}
+
+function push_message(e) {
+  let d = new Date(e.sent_at);
+  let node = document.createElement("div");
+  node.className = "mb-3";
+  node.innerHTML = `
+  <div class="text-xs">
+    <span class=" font-semibold">${d.toDateString()}</span>&nbsp;${e.sender_id}
+  </div>
+  <div class="text-md">${e.content}</div>
+  `;
+
+  message_pane.appendChild(node);
+  message_pane.scrollTo(0, message_pane.scrollHeight);
+}
+
+function add() {
+  open_chat(document.getElementById("user-id").value);
+}
+
+function open_chat(id) {
+  conn.send(
+    JSON.stringify({
+      sent_at: new Date(Date.now()).toISOString(),
+      tag: `join=${id}`,
+    })
+  );
+
+  fetch(`http://localhost:8000/api/v1/chats/${id}`, {
+    method: "GET",
+    credentials: "include",
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      message_pane.innerHTML = "";
+
+      res.forEach((e) => {
+        push_message(e);
+      });
+    });
+}
+
+function get_all_chats() {
+  message_pane.innerHTML = "";
+
+  let users = document.getElementById("users");
+  users.innerHTML = "";
+
+  fetch("http://localhost:8000/api/v1/chats", {
+    method: "GET",
+    credentials: "include",
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      res.forEach((e) => {
+        let node = document.createElement("div");
+        node.className =
+          "border-t border-gray-400 flex flex-row select-none cursor-pointer hover:bg-gray-200";
+        node.onclick = () => open_chat(e.user_id);
+        node.innerHTML = `
+        <div>
+          <div class="text-xs w-full">${e.user_id}</div>
+          <div class="text-xs w-full">${e.content}</div>
+        </div>
+        <div class="text-xs w-4 flex items-center justify-center">${e.unread_messages}</div>
+        `;
+
+        users.appendChild(node);
+      });
+    })
+    .catch((err) => console.error(err));
 }
 
 function connect() {
-  var msg = document.getElementById("msg");
-  var log = document.getElementById("log");
-
-  function appendLog(item) {
-    var doScroll = log.scrollTop > log.scrollHeight - log.clientHeight - 1;
-    log.appendChild(item);
-    if (doScroll) {
-      log.scrollTop = log.scrollHeight - log.clientHeight;
-    }
-  }
-
   if (window["WebSocket"]) {
     conn = new WebSocket(`ws://localhost:8000/ws/chats`);
 
     conn.onopen = function (evt) {
-      var item = document.createElement("div");
-      item.innerHTML = "<b>Connected.</b>";
-      appendLog(item);
+      get_all_chats();
+
+      let node = document.createElement("div");
+      node.innerHTML = "<b>Connected.</b>";
+      message_pane.appendChild(node);
     };
 
     conn.onclose = function (evt) {
       console.log(evt.reason);
       var item = document.createElement("div");
       item.innerHTML = "<b>Connection closed.</b>";
-      appendLog(item);
+      message_pane.appendChild(item);
     };
 
     conn.onmessage = function (evt) {
-      console.log(evt);
+      console.log(evt.data);
       var msg = JSON.parse(evt.data);
-      var item = document.createElement("div");
-      // item.innerHTML = `<b>${new Date(msg.created_at).toLocaleTimeString()} ${
-      //   msg.sender.username
-      // }</b>: ${msg.content}`;
-      item.innerHTML = evt.data;
-      appendLog(item);
+      push_message(msg);
     };
-  } else {
-    var item = document.createElement("div");
-    item.innerHTML = "<b>Your browser does not support WebSockets.</b>";
-    appendLog(item);
   }
 }
 
@@ -78,5 +132,7 @@ function login() {
       email: document.getElementById("user").value,
       password: document.getElementById("pass").value,
     }),
-  }).then((res) => console.log(res));
+  })
+    .then((res) => res.json())
+    .then((res) => console.log(res));
 }
