74d48b5bd7d6fabfff2221823bbf9b881ec0d713
Thanapat Ussawanarong <thanapat.usw@gmail.com>
2024-02-25 20:04:55 +0700

    refactor: get all chats

--- internal/core/chats/chat.repository.go
+++ internal/core/chats/chat.repository.go
@@ -1,6 +1,8 @@
 package chats
 
 import (
+	"database/sql"
+
 	"github.com/brain-flowing-company/pprp-backend/internal/models"
 	"github.com/google/uuid"
 	"gorm.io/gorm"
@@ -23,6 +25,39 @@ func NewRepository(db *gorm.DB) Repository {
 }
 
 func (repo *repositoryImpl) GetAllChats(results *[]models.ChatsResponses, userId uuid.UUID) error {
+	_ = repo.db.Model(&models.Messages{}).
+		Raw(`
+		SELECT *
+			FROM (
+				SELECT
+					DISTINCT ON (user_id) a.user_id,
+					COALESCE(unread_messages, 0) AS unread_messages,
+					content, sent_at
+				FROM (
+					SELECT 
+						CASE
+							WHEN sender_id = @user_id THEN receiver_id
+							WHEN receiver_id = @user_id THEN sender_id 
+						END AS user_id, message_id, content, read_at, sent_at
+					FROM messages
+					WHERE sender_id = @user_id OR receiver_id = @user_id
+					ORDER BY sent_at DESC
+				) AS a
+				LEFT JOIN (
+					SELECT
+						CASE
+							WHEN sender_id = @user_id THEN receiver_id
+							WHEN receiver_id = @user_id THEN sender_id
+						END AS user_id, count(*) AS unread_messages
+					FROM messages
+					WHERE receiver_id = @user_id AND read_at IS NULL
+					GROUP BY user_id
+				) AS b
+				ON a.user_id = b.user_id
+			) AS c
+		ORDER BY unread_messages DESC, sent_at DESC
+		`, sql.Named("user_id", userId)).
+		Scan(results).Error
 	return nil
 }
 
--- internal/models/chats.model.go
+++ internal/models/chats.model.go
@@ -7,8 +7,8 @@ import (
 )
 
 type ChatsResponses struct {
-	UnreadCount int64     `json:"unread_count"   example:"9"`
-	SenderId    uuid.UUID `json:"sender_id"      example:"123e4567-e89b-12d3-a456-426614174000"`
-	CreatedAt   time.Time `json:"created_at"     example:"2024-02-22T03:06:53.313735Z"`
-	Content     string    `json:"latest_message" example:"hello, world"`
+	UnreadMessages int64     `json:"unread_messages" example:"9"`
+	UserId         uuid.UUID `json:"user_id"         example:"123e4567-e89b-12d3-a456-426614174000"`
+	SentAt         time.Time `json:"sent_at"         example:"2024-02-22T03:06:53.313735Z"`
+	Content        string    `json:"content"         example:"hello, world"`
 }
