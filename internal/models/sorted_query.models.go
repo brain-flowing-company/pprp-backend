1f32ef0468486b862e0826ef2a2bd4eb5511026b
Thanapat Ussawanarong <thanapat.usw@gmail.com>
2024-03-14 12:02:47 +0700

    feat: sorted query

--- internal/models/sorted_query.models.go
+++ internal/models/sorted_query.models.go
@@ -1,16 +1,60 @@
 package models
 
-import "strings"
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strings"
+
+	"github.com/brain-flowing-company/pprp-backend/internal/enums"
+	"gorm.io/gorm"
+)
 
 type sorter struct {
-	field     string
-	direction string
+	Field     string
+	Direction enums.SortDirection
+	mapper    map[string]string
 }
 
-func NewSortedQuery(sort string) *sorter {
-	pairs := strings.Split(sort, ":")
-	return &sorter{
-		field:     pairs[0],
-		direction: pairs[1],
+func NewSortedQuery(t reflect.Type) *sorter {
+	s := &sorter{mapper: map[string]string{}}
+	for i := 0; i < t.NumField(); i++ {
+		f := t.Field(i)
+
+		json := f.Tag.Get("json")
+		sortmap := f.Tag.Get("sortmapper")
+
+		if len(json) > 0 && len(sortmap) > 0 {
+			s.mapper[json] = sortmap
+		}
 	}
+
+	return s
+}
+
+func (s *sorter) Parse(query string) error {
+	pairs := strings.Split(query, ":")
+
+	field, ok := s.mapper[pairs[0]]
+	if !ok {
+		return errors.New("invalid sort field")
+	}
+
+	direction, ok := enums.ParseSortDirection(pairs[1])
+	if !ok {
+		return errors.New("sort direction can only be 'asc' or 'desc'")
+	}
+
+	s.Field = field
+	s.Direction = direction
+
+	return nil
+}
+
+func (s *sorter) Map(key string, value string) {
+	s.mapper[key] = value
+}
+
+func (s *sorter) SortedQuery(db *gorm.DB) *gorm.DB {
+	return db.Order(fmt.Sprintf("%s %s", s.Field, s.Direction))
 }
