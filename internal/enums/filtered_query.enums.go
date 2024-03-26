86cdadd9661f490a80b65c31ac950293ce29ab6e
Thanapat Ussawanarong <thanapat.usw@gmail.com>
2024-03-26 14:51:43 +0700

    feat: add equal filter

--- internal/enums/filtered_query.enums.go
+++ internal/enums/filtered_query.enums.go
@@ -5,12 +5,14 @@ type FilterOperation string
 const (
 	GTE FilterOperation = ">="
 	LTE FilterOperation = "<="
+	EQL FilterOperation = "="
 )
 
 func ParseFilterOperation(dir string) (FilterOperation, bool) {
 	val, ok := map[string]FilterOperation{
 		"gte": GTE,
 		"lte": LTE,
+		"eql": EQL,
 	}[dir]
 	return val, ok
 }
