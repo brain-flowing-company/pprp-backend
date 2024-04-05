cd0100ac7b9eec40bbf122924bb07d6e411fec17
Krittapas Rungsimontuchat <nineten609@gmail.com>
2024-04-05 22:55:18 +0700

    feat : rating enums

---
+++ internal/enums/review_rating.enums.go
@@ -0,0 +1,17 @@
+package enums
+
+type Rating string
+
+const (
+	Rating0   Rating = "Rating0"
+	Rating0_5 Rating = "Rating0_5"
+	Rating1   Rating = "Rating1"
+	Rating1_5 Rating = "Rating1_5"
+	Rating2   Rating = "Rating2"
+	Rating2_5 Rating = "Rating2_5"
+	Rating3   Rating = "Rating3"
+	Rating3_5 Rating = "Rating3_5"
+	Rating4   Rating = "Rating4"
+	Rating4_5 Rating = "Rating4_5"
+	Rating5   Rating = "Rating5"
+)
