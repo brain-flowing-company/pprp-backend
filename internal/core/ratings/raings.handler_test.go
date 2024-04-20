0effc5e9189abba99ed9f33d6c9327798065572e
Krittapas Rungsimontuchat <nineten609@gmail.com>
2024-04-20 16:59:47 +0700

    feat : handler create unit test

--- internal/core/ratings/raings.handler_test.go
+++ internal/core/ratings/raings.handler_test.go
@@ -22,103 +22,9 @@ func (h *mockService) CreateRating(rating *models.Reviews) error {
 	return h.createrating(rating)
 }
 
-// func TestGetAllRatings(t *testing.T) {
-// 	testCase := []struct {
-// 		name     string
-// 		route    string
-// 		err      *apperror.AppError
-// 		expected int
-// 	}{
-// 		{
-// 			name:     "success",
-// 			err:      nil,
-// 			expected: 200,
-// 			route:    "/api/v1/ratings",
-// 		},
-// 	}
-// 	app := fiber.New()
-// 	// Create route with GET method for test
-// 	app.Get("/hello", func(c *fiber.Ctx) error {
-// 		// Return simple string as response
-// 		return c.SendString("Hello, World!")
-// 	})
-// 	for _, tc := range testCase {
-// 		t.Run(tc.name, func(t *testing.T) {
-// 			service := mockService{
-// 				getallrating: func(rating *[]models.RatingResponse) error {
-// 					return tc.err
-// 				},
-// 			}
-// 			handler := NewHandler(&service)
-// 			fmt.Println("handler = ", handler)
-
-// 			// Create a new HTTP request
-// 			app.Get(tc.route, handler.GetAllRatings)
-// 			req := httptest.NewRequest(http.MethodGet, tc.route, nil)
-// 			req.Header.Set("Content-Type", "application/json")
-
-// 			resp, err := app.Test(req)
-// 			if err != nil {
-// 				t.Fatal(err)
-// 			}
-// 			assert.Equal(t, resp.StatusCode, tc.expected)
-
-// 		})
-// 	}
-
-// }
-//
-//	{
-//		"property_id" : "0bd03187-91ac-457d-957c-3ba2f6c0d24b",
-//		"rating" : 1,
-//		"review" : "kuayfkdfjl"
-//	 }
-
-// func TestCreateRating(t *testing.T) {
-// 	app := fiber.New()
-// 	testCase := []struct {
-// 		description  string
-// 		requestbody  string
-// 		expectStatus int
-// 		cookieValue  string
-// 	}{
-// 		{
-// 			description: "success",
-// 			requestbody: `{
-// 				"property_id" : "0bd03187-91ac-457d-957c-3ba2f6c0d24b",
-// 				"rating" : 1,
-// 				"review" : "kuayfkdfjl"
-// 			}`,
-// 			expectStatus: fiber.StatusOK,
-// 			cookieValue:  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MTM2MjM1MjIsImlhdCI6MTcxMzUzNzEyMiwic2Vzc2lvbiI6eyJ1c2VyX2lkIjoiZGM0YmQwOTAtMWM3OS00N2M1LTlmOWQtNzUyOGZlMGMyOWQxIiwiZW1haWwiOiJuaW5ldGVuNjA5QGdtYWlsLmNvbSJ9fQ.mr4gvZFPICkrnv0r3Tter1r43Uv4S27hue2WZYm8PgA",
-// 		},
-// 	}
-// 	for _, tc := range testCase {
-// 		t.Run(tc.description, func(t *testing.T) {
-// 			reqBody, _ := json.Marshal(tc.requestbody)
-// 			service := mockService{
-// 				createrating: func(rating *models.Reviews) error {
-// 					return nil
-// 				},
-// 			}
-// 			handler := NewHandler(&service)
-// 			app.Post("/api/v1/ratings", handler.CreateRating)
-// 			req := httptest.NewRequest("POST", "/api/v1/ratings", bytes.NewReader(reqBody))
-// 			req.Header.Set("Content-Type", "application/json")
-// 			// Set session cookie
-// 			req.AddCookie(&http.Cookie{Name: "session", Value: tc.cookieValue})
-// 			resp, _ := app.Test(req)
-// 			if resp.StatusCode != tc.expectStatus {
-// 				t.Fatalf("expected %d but got %d", tc.expectStatus, resp.StatusCode)
-// 			}
-
-// 		})
-// 	}
-
-// }
-
 func TestCreateRating(t *testing.T) {
 	app := fiber.New()
+
 	testCase := []struct {
 		description  string
 		requestbody  string
@@ -129,21 +35,37 @@ func TestCreateRating(t *testing.T) {
 			description: "success",
 			requestbody: `{
                 "property_id" : "0bd03187-91ac-457d-957c-3ba2f6c0d24b",
-                "rating" : 1,
+                "rating" : 0,
                 "review" : "kuayfkdfjlklfjakl"
             }`,
 			expectStatus: fiber.StatusOK,
-			cookieValue:  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MTM2MjUxODAsImlhdCI6MTcxMzUzODc4MCwic2Vzc2lvbiI6eyJ1c2VyX2lkIjoiZmZhNWZmNjItNGVmMi00ODAzLWEwOTktNGYyMzliNTE5MmRkIiwiZW1haWwiOiJuaW5ldGVuNjA5QGdtYWlsLmNvbSJ9fQ.pXZ0wOneV98F1i69VzFu2Ukfe7xfzlsx3g_nCpyoqgw",
 		},
 		{
-			description: "failed",
+			description: "rating is not integer",
 			requestbody: `{
 				"property_id" : "0bd03187-91ac-457d-957c-3ba2f6c0d24b",
-				"rating" : 1,
+				"rating" : 1.5,
 				"review" : "kuayfkdfjl"
 			}`,
-			expectStatus: fiber.StatusUnauthorized,
-			cookieValue:  "",
+			expectStatus: fiber.StatusBadRequest,
+		},
+		{
+			description: "rating is not in range 1",
+			requestbody: `{
+				"property_id" : "0bd03187-91ac-457d-957c-3ba2f6c0d24b",
+				"rating" : 6,
+				"review" : "kuayfkdfjl"
+			}`,
+			expectStatus: fiber.StatusBadRequest,
+		},
+		{
+			description: "request body is not valid",
+			requestbody: `{
+				"property_id" : "0bd03187-91ac-457d-957c-3ba2f6c0d24b",
+				"rating" : 5,
+				"review" : "kuayfkdfjl"
+			`,
+			expectStatus: fiber.StatusBadRequest,
 		},
 	}
 	for _, tc := range testCase {
@@ -158,9 +80,8 @@ func TestCreateRating(t *testing.T) {
 			app.Post("/api/v1/ratings", handler.CreateRating)
 			req := httptest.NewRequest("POST", "/api/v1/ratings", bytes.NewReader(reqBody))
 			req.Header.Set("Content-Type", "application/json")
-
-			// Set session cookie
 			req.AddCookie(&http.Cookie{Name: "session", Value: tc.cookieValue})
+
 			resp, _ := app.Test(req)
 			if resp.StatusCode != tc.expectStatus {
 				t.Fatalf("expected %d but got %d", tc.expectStatus, resp.StatusCode)
